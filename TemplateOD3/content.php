14c1aab7049f2be456c81e0d79aeaa61a71a6556
EnesT51 <99040669@mydavinci.nl>
2023-04-04 23:57:23 +0200

    used include function to display content on page

---
+++ TemplateOD3/content.php
@@ -0,0 +1,21 @@
+<?php
+
+   for($i = 0; $i < 11; $i++){
+      $voornaam = "Enes";
+      $achternaam = "Tekinbas";
+      $img = "img/landscape.jpg";
+      echo "<h1>Peaceful place</h1>";
+      echo "<h2> $voornaam $achternaam</h2>";
+      echo "<img src='$img' style='width: 1000px; height: 600px;'>";
+      echo "<p>Japan is three times more densely populated than Europe and 12 times more than the U.S.<br>
+            Yet it still manages to ranks highly for both peace and quality of life.<br>
+            Theft and other felonies, the National Police Agency notes, have become passé.<br> 
+            Over the past few years, the number of recorded crimes continued to decrease to historically low levels—a trend also reflected in the low incarceration rate,<br>
+            which in Japan has followed a downward trajectory starting from the 1950s.<br><br>
+            However, when it comes to relations with neighboring countries, rising tensions with China and North Korea are often mentioned by the Japanese as areas of concern.<br>
+            Japan’s “peace constitution”—put in place following the Second World War to prohibit the resurrection of aggressive militarism—was reinterpreted in 2014 to enable “collective self-defense,”<br>
+            which preceded a restructuring and build-up of the country’s military capabilities.<br>
+            Low marks in the per-capita military expenditure and international conflict domains keep Japan far from the very top positions of the peace ranking.</p>";
+   }
+   
+?>
